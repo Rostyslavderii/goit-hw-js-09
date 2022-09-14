90c0865ceffb69d796cbc41935bc6a175013da2a
Rostyslavderii <105818935+Rostyslavderii@users.noreply.github.com>
2022-09-14 14:42:13 +0300

    done

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,28 @@
+
+let randomColor = function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+const body = document.querySelector('body');
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
+
+let timeOut = null;
+
+buttonStart.addEventListener('click', onButtonStartClick);
+buttonStop.addEventListener('click', onButtonStopClick);
+
+function onButtonStartClick(e) {
+  e.target.disabled = true;
+  buttonStop.disabled = false;
+  timeOut = setInterval(
+    () => (body.style.backgroundColor = randomColor()),
+    1000
+  );
+}
+
+function onButtonStopClick(e) {
+  e.target.disabled = true;
+  buttonStart.disabled = false;
+  clearInterval(timeOut);
+}
---
+++ src/js/03-promises.js
@@ -0,0 +1,44 @@
+import Notiflix from 'notiflix';
+const formSubmit = document.querySelector('.form');
+
+const delay = document.querySelector('[name="delay"]');
+const step = document.querySelector('[name="step"]');
+const amount = document.querySelector('[name="amount"]');
+
+let position = null;
+
+function createPromise(position, delay) {
+  const promise = new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+  promise
+    .then(({ position, delay }) => {
+      Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+    });
+}
+
+formSubmit.addEventListener('submit', onClickSubmit);
+
+function onClickSubmit(e) {
+  e.preventDefault();
+
+  const { delay, step, amount } = e.target.elements;
+
+  for (let i = 1; i <= amount.value; i += 1) {
+    position += 1;
+    delay.value = Number(step.value) + Number(delay.value);
+    createPromise(position, delay.value);
+  }
+  position = 0;
+  e.target.reset();
+}
